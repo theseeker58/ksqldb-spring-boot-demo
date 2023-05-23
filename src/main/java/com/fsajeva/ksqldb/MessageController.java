99210d85df185e6ae17953fa323242017f962447
TheSeeker58 <theseeker58@gmail.com>
2023-05-23 20:48:14 +0200

    Updated libreries Refactored GET method

--- src/main/java/com/fsajeva/ksqldb/MessageController.java
+++ src/main/java/com/fsajeva/ksqldb/MessageController.java
@@ -3,6 +3,8 @@ package com.fsajeva.ksqldb;
 import io.confluent.ksql.api.client.BatchedQueryResult;
 import io.confluent.ksql.api.client.Client;
 import io.confluent.ksql.api.client.Row;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,6 @@ public class MessageController {
     }
 
     @GetMapping("/")
-    //public String findAll() throws ExecutionException, InterruptedException {
     public List<Message> findAll() throws ExecutionException, InterruptedException {
         String pullQuery = "SELECT * FROM MESSAGES;";
         BatchedQueryResult batchedQueryResult = ksqlClient.executeQuery(pullQuery);
@@ -29,7 +30,6 @@ public class MessageController {
         for (Row row : resultRows) {
             System.out.println("Row: " + row.values());
         }
-        //return "The query has returned " + resultRows.size() + " messages";
         return map(resultRows);
     }
 
@@ -42,16 +42,36 @@ public class MessageController {
         List<Row> resultRows = batchedQueryResult.get();
         return map(resultRows);
     }
+    @GetMapping("/message/{sender}/{sequence}")
+    //public Message findOne(@PathVariable String sender, @PathVariable String sequence) throws ExecutionException, InterruptedException {
+    public ResponseEntity<Message> findOne(@PathVariable String sender, @PathVariable String sequence) throws ExecutionException, InterruptedException {
+        ksqlClient.define("sender", sender);
+        ksqlClient.define("sequence", sequence);
+        String pullQuery = "SELECT * FROM MESSAGES WHERE SENDER = '${sender}' AND SEQUENCE ='${sequence}';";
+        BatchedQueryResult batchedQueryResult = ksqlClient.executeQuery(pullQuery);
+        List<Row> resultRows = batchedQueryResult.get();
+        Message message = null;
+        if (resultRows.size() == 1) {
+            Row row = resultRows.get(0);
+            message = map(row);
+            return ResponseEntity.ok().body(message);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
 
     private List<Message> map(List<Row> resultRows) {
         List<Message> messages = new ArrayList<>();
         for (Row row : resultRows) {
-            Message message = new Message();
-            message.setSender(row.getString("SENDER"));
-            message.setSequence(row.getString("SEQUENCE"));
-            message.setText(row.getString("TEXT"));
-            messages.add(message);
+            messages.add(map(row));
         }
         return messages;
     }
+    private Message map(Row row) {
+        Message message = new Message();
+        message.setSender(row.getString("SENDER"));
+        message.setSequence(row.getString("SEQUENCE"));
+        message.setText(row.getString("TEXT"));
+        return message;
+    }
+
 }
